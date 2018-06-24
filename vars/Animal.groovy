d907e6d529123de330191a25a38db92be206a768
%%GITNAME%% <%%GITEMAIL%%>
2018-06-24 08:55:01 +0800

    regular commit

--- vars/Animal.groovy
+++ vars/Animal.groovy
@@ -1,4 +1,13 @@
+#!/usr/bin/groovy
 
-def call(Map config) {
+
+def call(body) {
+
+    // evaluate the body block, and collect configuration into the object
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
     echo "Dogs is eating ${config.food}"
 }
