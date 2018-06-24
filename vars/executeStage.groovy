1d8e0f4ab3c26448297bae3d88a78b201ef00424
%%GITNAME%% <%%GITEMAIL%%>
2018-06-24 09:26:02 +0800

    regular commit

--- vars/executeStage.groovy
+++ vars/executeStage.groovy
@@ -1,4 +1,10 @@
 def call(Closure body) {
+    // evaluate the body block, and collect configuration into the object
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
     stage(config.name) {
         config.cmd
     }
