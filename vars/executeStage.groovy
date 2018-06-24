df71d5363356b4fa98767e7e6accae64298299f9
%%GITNAME%% <%%GITEMAIL%%>
2018-06-24 11:11:37 +0800

    regular commit

--- vars/executeStage.groovy
+++ vars/executeStage.groovy
@@ -4,6 +4,8 @@ def call(String name, Closure body) {
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
 
+    def library = pwd() + '/workflows/Animal/library/Animal.groovy'
+    lib = load "${library}"
 
     stage(name) {
         println("""
